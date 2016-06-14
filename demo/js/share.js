c91299e9214254b4b6565dace9ac1f1b5fcc6dac
ShonLin <tsunamibad@qq.com>
2016-06-14 17:22:02 +0800

    demo修改

--- demo/js/share.js
+++ demo/js/share.js
@@ -1,5 +1,5 @@
 wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: '{$appid}', // 必填，公众号的唯一标识
     timestamp: "{$signPackage.timestamp}", // 必填，生成签名的时间戳
     nonceStr: '{$signPackage.nonceStr}', // 必填，生成签名的随机串
@@ -31,14 +31,6 @@ wx.ready(function () {
     });
 });
 
-// wx.config({
-//     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-//     appId: '', // 必填，公众号的唯一标识
-//     timestamp: , // 必填，生成签名的时间戳
-//     nonceStr: '', // 必填，生成签名的随机串
-//     signature: '',// 必填，签名，见附录1
-//     jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-// });
 // wx.onMenuShareTimeline({
 //     title: '测测你的少女值吧~', // 分享标题
 //     link: 'http://shonlin.github.io/WeeklyDiscussionsRecord/demo/index.html', // 分享链接
